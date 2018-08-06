7e3f4f1532c8616b92d96bfeec885195af501d8b
twitis001 <jem_munar@yahoo.com>
2018-08-06 08:21:54 +0800

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -10,15 +10,60 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'UTF8');
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'UTF8');
+
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
     res.render(
       'index',
       {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts
+      }
+    );
+  });
+
+  app.get('/savings', (req, res) => {
+    res.render(
+      'account',
+      {
+        account: accounts.savings
+      }
+    );
+  });
+
+  app.get('/checking', (req, res) => {
+    res.render(
+      'account',
+      {
+        account: accounts.checking
+      }
+    );
+  });
+
+  app.get('/credit', (req, res) => {
+    res.render(
+      'account',
+      {
+        account: accounts.credit
+      }
+    );
+  });
+
+  app.get('/profile', (req, res) => {
+    res.render(
+      'profile',
+      {
+        user: users[0]
       }
     );
   });
 
   app.listen(3000, () => {
-    debug('PS Project Running on port 3000!');
+    console.log('PS Project Running on port 3000!');
   });
